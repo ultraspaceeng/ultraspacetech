7409b5d5efa4d353c9880be5396e103de97b758e
Akinleyejoshua <akinleyejoshua.dev@gmail.com>
2026-03-18 10:25:01 +0100

    n

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ const TEAM_CONTENT = {
             name: 'Joshua Akinleye',
             role: 'CEO & Founder, Software Developer',
             image: 'https://ultraspacetech.vercel.app/team-ceo.jpg',
-            socials: { website: "https://akinleyejoshua.vercel.app/", twitter: 'https://x.com/Joshuaakinleye4', linkedin: 'https://www.linkedin.com/in/joshua-a-9895b61ab/', github: 'https://github.com/Akinleyejoshua' }
+            socials: { website: "https://joshuapro.netlify.app/", twitter: 'https://x.com/Joshuaakinleye4', linkedin: 'https://www.linkedin.com/in/joshua-a-9895b61ab/', github: 'https://github.com/Akinleyejoshua' }
         },
     ]
 };
